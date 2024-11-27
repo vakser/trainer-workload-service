d40ce349b316836eb601843846fa3d528b1b56cf
vakser <vakals@gmail.com>
2024-11-27 20:12:53 +0200

    Initial commit

---
+++ src/main/java/com/epam/learn/trainerworkloadservice/TrainerWorkloadServiceApplication.java
@@ -0,0 +1,13 @@
+package com.epam.learn.trainerworkloadservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrainerWorkloadServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TrainerWorkloadServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/epam/learn/trainerworkloadservice/TrainerWorkloadServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.epam.learn.trainerworkloadservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrainerWorkloadServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
